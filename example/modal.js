beb4a2f885af50f12ffc288c556b55fddb36a826
Martin Kmieciak <mkmieciak@apptension.com>
2018-09-20 11:55:40 +0200

    Remove unnecessary state and divs

--- example/modal.js
+++ example/modal.js
@@ -1,56 +1,37 @@
 import React from 'react';
-import { WithRouter, PortalRoute } from '../src';
+import { Link, PortalRoute } from '../src';
 
 export class Modal extends React.PureComponent {
   state = {
-    counter: 0,
     showWindowPortal: false,
   };
 
-  componentDidMount() {
-    window.addEventListener('beforeunload', () => {
-      this.closeWindowPortal();
-    });
-  }
-
-  toggleWindowPortal = () => {
-    this.setState(state => ({
-      ...state,
-      showWindowPortal: !state.showWindowPortal,
-    }));
-
-    window.setTimeout(() => {
-      this.forceUpdate();
-    }, 100);
-  };
+  toggleWindowPortal = () => this.setState(state => ({
+    showWindowPortal: !state.showWindowPortal,
+  }));
 
   closeWindowPortal = () => this.setState({ showWindowPortal: false });
 
+  handlePortalCreate = () => this.forceUpdate();
+
   render() {
     return (
-      <div>
+      <React.Fragment>
         <button className="route__button route__button--light" onClick={this.toggleWindowPortal}>
           {this.state.showWindowPortal ? 'Close the' : 'Open a'} Portal
         </button>
 
         {this.state.showWindowPortal && (
-          <PortalRoute>
+          <PortalRoute onCreate={this.handlePortalCreate}>
             <div className="route__container route__container--dark">
-
-              <WithRouter>
-                {({ push }) => (
-                  <button className="route__button route__button--light" onClick={() => push('product/2')} >
-                    Change route in the app
-                  </button>
-                )}
-              </WithRouter>
+              <Link className="route__button route__button--light" to="/product/2">Change route</Link>
               <button className="route__button route__button--light" onClick={() => this.closeWindowPortal()} >
                 Close me!
               </button>
             </div>
           </PortalRoute>
         )}
-      </div>
+      </React.Fragment>
     );
   }
 }
